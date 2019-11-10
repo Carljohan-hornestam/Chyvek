351998ba284f41d4223a9720d87ab5957d0db43a
codeItRalf <codeItRalf@users.noreply.github.com>
2019-11-10 22:25:10 +0100

    Added sub nav to navbar

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ $(".carousel-control-prev").click(function(){
     $("#boring-jobs").carousel("prev");
   });
 
-function orderAccepted() {
-    alert("Your order has been registered! Thank you for choosing Chyvek!")
-}
+
 
 let hideText = document.querySelector('#hidden-text');
 let silverPackage = document.querySelector('#package-icon');
